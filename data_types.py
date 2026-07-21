4623dcb36311364c19d0a6709620c030233c2413
gratan09 <gunnalaratan@gmail.com>
2026-07-22 02:21:03 +0530

    changed

---
+++ data_types.py
@@ -0,0 +1,32 @@
+#integer
+age = 19
+
+print(age)
+
+#float
+
+value_of_pi = 3.14
+
+print(value_of_pi)
+
+#String
+
+name = "Amber"
+
+print(name)
+
+#boolean
+
+is_student = True
+
+print(is_student)
+
+is_sportsman = False
+
+print(is_sportsman)
+
+
+#nontype
+
+phone_number = None
+print(phone_number)
