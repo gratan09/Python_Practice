81992e76a42023562953edd153a58ce7c83fbeba
gratan09 <gunnalaratan@gmail.com>
2026-07-22 02:35:45 +0530

    data types and type function

--- data_types1.py
+++ data_types1.py
@@ -12,3 +12,16 @@ print(type(age))
 
 age1 = 23
 print(type(age1))
+
+
+# assignment
+
+book_name = "rich dad poor dad"
+book_price = 265
+is_avaliable = True
+author = "Robert T. Kiyosaki"
+
+print(type(book_name))
+print(type(book_price))
+print(type(is_avaliable))
+print(type(author))
