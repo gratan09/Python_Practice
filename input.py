8f13f1f07e95b5aa8b411af0e6e42f75f5b084fd
gratan09 <gunnalaratan@gmail.com>
2026-07-22 03:17:41 +0530

    assignment

--- input.py
+++ input.py
@@ -13,3 +13,37 @@ print(age)
 city = input("Enter your city: ")
 print(city)
 
+#fstrings
+
+name = input("Enter your name: ")
+
+age = int(input("Enter you age: "))
+
+print(f"Hello, {name}")
+print(f"you are {age} old")
+
+
+
+age = int(input("enter you age: "))
+
+
+print(age + 10)
+
+
+#practice_assignment
+
+name = input("Enter Name: ")
+age = int(input("Enter Age: "))
+college = input("Enter College Name: ")
+branch = input("Enter Branch: ")
+height = float(input("Enter Height: "))
+
+
+
+
+print("---- Student Details ----")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"College: {college}")
+print(f"Branch: {branch}")
+print(f"Height: {height}")
