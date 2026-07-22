2286036338cc88697a8154dcad582e2a43cc89f4
gratan09 <gunnalaratan@gmail.com>
2026-07-22 23:45:15 +0530

    operators prac

---
+++ operators.py
@@ -0,0 +1,42 @@
+#arthemetic operators
+
+a = 5
+b = 12
+
+print(a+b)
+
+
+a = 10
+b = 7
+
+print(a-b)
+
+
+a = 6
+b = 4
+
+print(a*b)
+
+
+a = 4
+b = 2
+
+print(a/b)
+
+
+a = 11
+b = 4
+
+print(a//b)
+
+
+a = 12
+b = 3
+
+print(a%b)
+
+
+a = 11
+b = 4
+
+print(a%b)
