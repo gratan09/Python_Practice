35f92dec0dc2c59985d8a7bb4a49b636a2f0590d
gratan09 <gunnalaratan@gmail.com>
2026-07-22 23:26:33 +0530

    22-07

---
+++ input1.py
@@ -0,0 +1,13 @@
+name = input("Enter Name: ")
+age = int(input("Enter Age: "))
+
+print(f"Hello, {name}")
+print(f"Next year you will be {age +1} years old.")
+
+#sum of three numbers
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+print(f"Sum is: {num1 + num2 + num3}")
