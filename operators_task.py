e951287bc7bd6f49a7338b22c1b8120c020ba326
gratan09 <gunnalaratan@gmail.com>
2026-07-24 01:47:04 +0530

    task_assignment

---
+++ operators_task.py
@@ -0,0 +1,34 @@
+#task 1
+"""
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print(f"The sum of given numbers is: {num1 + num2}")
+print(f"The difference of the given numbers is: {num1 - num2}")
+print(f"The product of the given numbers is: {num1 * num2}")
+print(f"The division of the given numbers is: {num1 / num2}")
+
+print(f"The floor division of the given number is: {num1 // num2}")
+print(f"The modulus of the given numbers is: {num1 % num2}")
+
+print(f"The power of the given numbers is: {num1**num2}")
+
+"""
+
+"""
+#task 2
+
+number1 = int(input("Enter the 1st number: "))
+number2 = int(input("Enter the 2nd number: "))
+
+print(f"is 1st number greater than 2nd number: {number1 > number2}")
+print(f"is 1st number smaller than 2nd number: {number1 < number2}")
+print(f" are they equal: {number1 == number2}")
+
+
+"""
+#task 3
+
+age = int(input("Enter your age: "))
+
+print(f"is age greater than 18?: {age > 18}")
