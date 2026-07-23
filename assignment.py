137be9b411cfcaad1299d8706d75e9739ed9ee37
gratan09 <gunnalaratan@gmail.com>
2026-07-24 01:19:43 +0530

    new

--- assignment.py
+++ assignment.py
@@ -9,3 +9,22 @@ x = x + 5
 x += 15
 
 print(x)
+
+
+
+# similar
+
+x = 10
+
+x += 5
+print(f"the result is {x}")
+x -= 5
+print(f"the result is {x}")
+x *= 5
+print(f"the result is {x}")
+
+
+
+x = 100
+x /= 5
+print(f"the result is {x}")
