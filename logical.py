0b59a3d18ac6f2dfda8641ec3611b2616f72d77d
gratan09 <gunnalaratan@gmail.com>
2026-07-24 01:11:01 +0530

    logical

--- logical.py
+++ logical.py
@@ -1,1 +1,18 @@
-print("vishnu vardhan")
+# AND , OR , NOT
+
+# AND both conditons must be true
+
+print( True and True)
+
+print( True and False)
+
+
+# OR Atleast one condition must be true
+
+print (True or False)
+
+
+# NOT Reverse the result
+
+print(not True)
+
