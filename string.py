a9d0422a0675328151091b72c9f1c88661459a9b
gratan09 <gunnalaratan@gmail.com>
2026-07-28 23:52:28 +0530

    updated

---
+++ string.py
@@ -0,0 +1,22 @@
+# String is simply a text
+
+# we can use string by using double quotes or single quotes
+
+
+name = "Kelvin"
+
+age = "Twenty Five"
+
+Hobbies = "Watching moveis"
+
+
+print(name)
+
+
+# accessing charectors
+
+name1 = "Alex"
+
+print(name1[0])
+
+print(name1[3])
