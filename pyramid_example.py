477266f57f98baede342f9c984b7ddb0d76dc32f
gratan09 <gunnalaratan@gmail.com>
2026-08-01 01:38:13 +0530

    pyramid

---
+++ pyramid_example.py
@@ -0,0 +1,13 @@
+
+
+
+def main():
+    N = 5
+    for i in range(1, N + 1):
+        for j in range(1, i + 1):
+            print("*", end="")
+
+        print()
+
+if __name__ == "__main__":
+    main()
